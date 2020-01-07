8ef5f635992a20e4eb23af31abcde87e037e8a5a
xu li <447629057@qq.com>
2020-01-07 22:10:51 +0800

    0107

--- components/list-comp/list-detailNav/list-detailNav.js
+++ components/list-comp/list-detailNav/list-detailNav.js
@@ -11,15 +11,43 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    details:[
+      {
+        label:'深呼吸',
+        checked:false
+      },
+      {
+        label: '限时特惠',
+        checked: false
+      },
+      {
+        label: '非首次出租',
+        checked: false
+      },
+      {
+        label: 'VR看房',
+        checked: false
+      },
+      {
+        label: '视频看房',
+        checked: false
+      },
+    ],
+    
+    
   },
-
+  
   /**
    * 组件的方法列表
    */
   methods: {
-    handChoice(){
-      console.log(this)
+    handChoice(e){
+      let index = e.currentTarget.dataset.index
+      let bool = this.data.details[index].checked
+      this.setData({
+        ['details[' + index + '].checked']:!bool
+      })
+      
     }
 
 
